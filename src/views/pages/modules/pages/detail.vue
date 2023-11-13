847586f729febed0f1636d0cf461b7e64a6917df
alexela8882 <alexela8882@gmail.com>
2023-11-13 09:29:25 +0800

    ui/ux improvements; loading state added in detail page

--- src/views/pages/modules/pages/detail.vue
+++ src/views/pages/modules/pages/detail.vue
@@ -16,7 +16,7 @@ const moduleStore = useModuleStore()
 const moduleDetailStore = useModuleDetailStore()
 const { fetchModule, fetchBaseModule } = moduleStore
 const { getModule, getFieldDetailsById } = storeToRefs(moduleStore)
-const { getItem, getItemPanels, getItemByName } = storeToRefs(moduleDetailStore)
+const { itemLoading, getItem, getItemPanels, getItemByName } = storeToRefs(moduleDetailStore)
 const { fetchItem } = moduleDetailStore
 // presets
 const bcrumbs = ref([
@@ -42,64 +42,72 @@ onMounted(async() => {
   <div>
     <RdBreadCrumbs :bcrumbs="bcrumbs" />
 
-    <div class="bg-white p-5 mb-5 border-round-xl">
-      <!-- <pre>{{ getModule.fields }}</pre> -->
+    <div
+      v-if="itemLoading"
+      class="flex align-items-center justify-content-center"
+      style="height: 60vh !important;">
+      <ProgressSpinner />
     </div>
+    <div v-else>
+      <div class="bg-white p-5 mb-5 border-round-xl">
+        <!-- <pre>{{ getModule.fields }}</pre> -->
+      </div>
 
-    <div class="grid">
-      <div class="col flex flex-column gap-3">
-        <div v-for="(panel, px) in getItemPanels" :key="px" class="flex flex-column gap-4">
-          <Panel :header="panel.entityName" class="detail-page-panel">
-            <template #header>
-              <div class="flex align-items-center justify-content-between w-full">
-                <div class="text-2xl font-bold">{{ panel.entityName }}</div>
-                <div v-if="panel.mutable">
-                  <div class="material-icons cursor-pointer">playlist_add</div>
+      <div class="grid">
+        <div class="col flex flex-column gap-3">
+          <div v-for="(panel, px) in getItemPanels" :key="px" class="flex flex-column gap-4">
+            <Panel :header="panel.entityName" class="detail-page-panel">
+              <template #header>
+                <div class="flex align-items-center justify-content-between w-full">
+                  <div class="text-2xl font-bold">{{ panel.entityName }}</div>
+                  <div v-if="panel.mutable">
+                    <div class="material-icons cursor-pointer">playlist_add</div>
+                  </div>
                 </div>
-              </div>
-            </template>
-            <div v-for="(section, sx) in panel.sections" :key="sx" class="pt-4">
-              <div class="flex flex-column gap-4">
-                <div class="text-lg text-primary font-bold">{{ section.sectionLabel }}</div>
-                <div class="grid">
-                  <div
-                    v-for="(field, fx) in section.field_ids"
-                    :key="fx"
-                    class="col flex flex-column gap-4">
-                    <div v-for="(id, idx) in field">
-                      <div v-if="getFieldDetailsById(id)" class="flex align-items-center gap-4">
-                        <div class="white-space-nowrap">{{ getFieldDetailsById(id).label }}</div>
-                        <div v-if="getFieldDetailsById(id).relation" class="flex gap-2">
-                          <div v-for="(displayField, dfx) in getFieldDetailsById(id).relation.displayFieldName" :key="dfx" class="font-bold">
-                            <div v-if="getItemByName(getFieldDetailsById(id).name)">
-                              <div v-if="typeof getItemByName(getFieldDetailsById(id).name) === 'object' &&
-                                          !Array.isArray(getItemByName(getFieldDetailsById(id).name)) &&
-                                          getItemByName(getFieldDetailsById(id).name) !== null">
-                                {{ getItemByName(getFieldDetailsById(id).name)[displayField] }}
+              </template>
+              <div v-for="(section, sx) in panel.sections" :key="sx" class="pt-4">
+                <div class="flex flex-column gap-4">
+                  <div class="text-lg text-primary font-bold">{{ section.sectionLabel }}</div>
+                  <div class="grid">
+                    <div
+                      v-for="(field, fx) in section.field_ids"
+                      :key="fx"
+                      class="col flex flex-column gap-4">
+                      <div v-for="(id, idx) in field">
+                        <div v-if="getFieldDetailsById(id)" class="flex align-items-start gap-4 border-bottom-1 border-200">
+                          <div class="white-space-nowrap">{{ getFieldDetailsById(id).label }}</div>
+                          <div v-if="getFieldDetailsById(id).relation" class="flex gap-2">
+                            <div v-for="(displayField, dfx) in getFieldDetailsById(id).relation.displayFieldName" :key="dfx" class="font-bold">
+                              <div v-if="getItemByName(getFieldDetailsById(id).name)">
+                                <div v-if="typeof getItemByName(getFieldDetailsById(id).name) === 'object' &&
+                                            !Array.isArray(getItemByName(getFieldDetailsById(id).name)) &&
+                                            getItemByName(getFieldDetailsById(id).name) !== null">
+                                  {{ getItemByName(getFieldDetailsById(id).name)[displayField] }}
+                                </div>
+                                <div v-else-if="Array.isArray(getItemByName(getFieldDetailsById(id).name))">
+                                  <Tag
+                                    v-for="(relationArr, rax) in getItemByName(getFieldDetailsById(id).name)"
+                                    :key="rax"
+                                    rounded
+                                    :value="relationArr[displayField]"
+                                    class="white-space-nowrap px-3 m-1 cursor-pointer" severity="info"></Tag>
+                                </div>
                               </div>
-                              <div v-else-if="Array.isArray(getItemByName(getFieldDetailsById(id).name))">
-                                <Tag
-                                  v-for="(relationArr, rax) in getItemByName(getFieldDetailsById(id).name)"
-                                  :key="rax"
-                                  rounded
-                                  :value="relationArr[displayField]"
-                                  class="white-space-nowrap px-3 m-1 cursor-pointer" severity="info"></Tag>
-                              </div>
+                              <div v-else></div>
                             </div>
-                            <div v-else></div>
                           </div>
-                        </div>
-                        <div v-else class="flex gap-4 font-bold">
-                          <div v-if="Array.isArray(getItemByName(getFieldDetailsById(id).name))">
-                            <Tag
-                              v-for="(item, itx) in getItemByName(getFieldDetailsById(id).name)"
-                              :key="itx"
-                              rounded
-                              :value="item"
-                              class="white-space-nowrap px-3 m-1 cursor-pointer my-1" severity="info"></Tag>
-                          </div>
-                          <div v-else>
-                            {{ getItemByName(getFieldDetailsById(id).name) ? getItemByName(getFieldDetailsById(id).name) : '' }}
+                          <div v-else class="flex gap-4 font-bold">
+                            <div v-if="Array.isArray(getItemByName(getFieldDetailsById(id).name))">
+                              <Tag
+                                v-for="(item, itx) in getItemByName(getFieldDetailsById(id).name)"
+                                :key="itx"
+                                rounded
+                                :value="item"
+                                class="white-space-nowrap px-3 m-1 cursor-pointer my-1" severity="info"></Tag>
+                            </div>
+                            <div v-else>
+                              {{ getItemByName(getFieldDetailsById(id).name) ? getItemByName(getFieldDetailsById(id).name) : '' }}
+                            </div>
                           </div>
                         </div>
                       </div>
@@ -107,14 +115,14 @@ onMounted(async() => {
                   </div>
                 </div>
               </div>
-            </div>
-          </Panel>
+            </Panel>
+          </div>
         </div>
+        <!-- <div class="col flex flex-column gap-3">
+          <div class="text-2xl">Item</div>
+          <div>{{ getItem }}</div>
+        </div> -->
       </div>
-      <!-- <div class="col flex flex-column gap-3">
-        <div class="text-2xl">Item</div>
-        <div>{{ getItem }}</div>
-      </div> -->
     </div>
   </div>
 </template>
