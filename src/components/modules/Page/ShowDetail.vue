f2d0b7061901bb8244974a8b7eded7b50d3b7a20
alexela8882 <alexela8882@gmail.com>
2023-11-14 11:22:25 +0800

    panels updated

--- src/components/modules/Page/ShowDetail.vue
+++ src/components/modules/Page/ShowDetail.vue
@@ -107,82 +107,81 @@ onMounted(async() => {
             <div>
               <div class="flex flex-column gap-3">
                 <div v-for="(panel, px) in localItemPanels" :key="px" class="flex flex-column gap-4">
-                  <Panel :header="panel.entityName" class="detail-page-panel">
-                    <template #header>
-                      <div class="flex align-items-center justify-content-between w-full">
-                        <div class="text-2xl font-bold">{{ panel.entityName }}</div>
-                        <div v-if="getRelatedListsByCname(panel.panelName)">
-                          <div class="material-icons cursor-pointer">playlist_add</div>
+                  <div v-for="(section, sx) in panel.sections" :key="sx" class="pt-4">
+                    <Panel class="detail-page-panel">
+                      <template #header>
+                        <div class="flex align-items-center justify-content-between w-full">
+                          <div class="text-2xl font-bold">{{ section.sectionLabel }}</div>
+                          <div v-if="getRelatedListsByCname(panel.panelName)">
+                            <div class="material-icons cursor-pointer">playlist_add</div>
+                          </div>
                         </div>
-                      </div>
-                    </template>
-                    <div v-for="(section, sx) in panel.sections" :key="sx" class="pt-4">
-                      <div class="flex flex-column gap-4">
-                        <div class="text-lg text-primary font-bold">{{ section.sectionLabel }}</div>
-                        <div v-if="getRelatedListsByCname(panel.panelName)">
-                          <Suspense v-if="getRelatedListsByCname(panel.panelName)">
-                            <DynamicDataTable
-                              :moduleId="panel._id"
-                              :moduleName="getRelatedListsByCname(panel.panelName).link"
-                              :moduleLabel="getRelatedListsByCname(panel.panelName).label"
-                              :fields="getRelatedListsByCname(panel.panelName).fields.data"
-                              :data="getRelatedListsByCname(panel.panelName).collection.data"
-                              :pagination="getRelatedListsByCname(panel.panelName).collection.meta"
-                              :collectionLoading="false">
-                            </DynamicDataTable>
-                            <template #fallback>
-                              <DataTableLoader />
-                            </template>
-                          </Suspense>
-                        </div>
-                        <div v-else class="grid">
-                          <div
-                            v-for="(field, fx) in section.field_ids"
-                            :key="fx"
-                            class="col flex flex-column gap-4">
-                            <div v-for="(id, idx) in field">
-                              <div v-if="getFieldDetailsById(id)" class="flex align-items-start gap-4 border-bottom-1 border-200">
-                                <div class="white-space-nowrap">{{ getFieldDetailsById(id).label }}</div>
-                                <div v-if="getFieldDetailsById(id).relation" class="flex gap-2">
-                                  <div v-for="(displayField, dfx) in getFieldDetailsById(id).relation.displayFieldName" :key="dfx" class="font-bold">
-                                    <div v-if="getItemValueByName(getFieldDetailsById(id).name)">
-                                      <div v-if="typeof getItemValueByName(getFieldDetailsById(id).name) === 'object' &&
-                                                  !Array.isArray(getItemValueByName(getFieldDetailsById(id).name)) &&
-                                                  getItemValueByName(getFieldDetailsById(id).name) !== null">
-                                        {{ getItemValueByName(getFieldDetailsById(id).name)[displayField] }}
+                      </template>
+                        <div class="flex flex-column gap-4 mt-4">
+                          <div v-if="getRelatedListsByCname(panel.panelName)">
+                            <Suspense v-if="getRelatedListsByCname(panel.panelName)">
+                              <DynamicDataTable
+                                :moduleId="panel._id"
+                                :moduleName="getRelatedListsByCname(panel.panelName).link"
+                                :moduleLabel="getRelatedListsByCname(panel.panelName).label"
+                                :fields="getRelatedListsByCname(panel.panelName).fields.data"
+                                :data="getRelatedListsByCname(panel.panelName).collection.data"
+                                :pagination="getRelatedListsByCname(panel.panelName).collection.meta"
+                                :collectionLoading="false">
+                              </DynamicDataTable>
+                              <template #fallback>
+                                <DataTableLoader />
+                              </template>
+                            </Suspense>
+                          </div>
+                          <div v-else class="grid">
+                            <div
+                              v-for="(field, fx) in section.field_ids"
+                              :key="fx"
+                              class="col flex flex-column gap-4">
+                              <div v-for="(id, idx) in field">
+                                <div v-if="getFieldDetailsById(id)" class="flex align-items-start gap-4 border-bottom-1 border-200">
+                                  <div class="white-space-nowrap">{{ getFieldDetailsById(id).label }}</div>
+                                  <div v-if="getFieldDetailsById(id).relation" class="flex gap-2">
+                                    <div v-for="(displayField, dfx) in getFieldDetailsById(id).relation.displayFieldName" :key="dfx" class="font-bold">
+                                      <div v-if="getItemValueByName(getFieldDetailsById(id).name)">
+                                        <div v-if="typeof getItemValueByName(getFieldDetailsById(id).name) === 'object' &&
+                                                    !Array.isArray(getItemValueByName(getFieldDetailsById(id).name)) &&
+                                                    getItemValueByName(getFieldDetailsById(id).name) !== null">
+                                          {{ getItemValueByName(getFieldDetailsById(id).name)[displayField] }}
+                                        </div>
+                                        <div v-else-if="Array.isArray(getItemValueByName(getFieldDetailsById(id).name))">
+                                          <Tag
+                                            v-for="(relationArr, rax) in getItemValueByName(getFieldDetailsById(id).name)"
+                                            :key="rax"
+                                            rounded
+                                            :value="relationArr[displayField]"
+                                            class="white-space-nowrap px-3 m-1 cursor-pointer" severity="info"></Tag>
+                                        </div>
                                       </div>
-                                      <div v-else-if="Array.isArray(getItemValueByName(getFieldDetailsById(id).name))">
-                                        <Tag
-                                          v-for="(relationArr, rax) in getItemValueByName(getFieldDetailsById(id).name)"
-                                          :key="rax"
-                                          rounded
-                                          :value="relationArr[displayField]"
-                                          class="white-space-nowrap px-3 m-1 cursor-pointer" severity="info"></Tag>
-                                      </div>
+                                      <div v-else></div>
                                     </div>
-                                    <div v-else></div>
                                   </div>
-                                </div>
-                                <div v-else class="flex gap-4 font-bold">
-                                  <div v-if="Array.isArray(getItemValueByName(getFieldDetailsById(id).name))">
-                                    <Tag
-                                      v-for="(item, itx) in getItemValueByName(getFieldDetailsById(id).name)"
-                                      :key="itx"
-                                      rounded
-                                      :value="item"
-                                      class="white-space-nowrap px-3 m-1 cursor-pointer my-1" severity="info"></Tag>
-                                  </div>
-                                  <div v-else>
-                                    {{ getItemValueByName(getFieldDetailsById(id).name) ? getItemValueByName(getFieldDetailsById(id).name) : '' }}
+                                  <div v-else class="flex gap-4 font-bold">
+                                    <div v-if="Array.isArray(getItemValueByName(getFieldDetailsById(id).name))">
+                                      <Tag
+                                        v-for="(item, itx) in getItemValueByName(getFieldDetailsById(id).name)"
+                                        :key="itx"
+                                        rounded
+                                        :value="item"
+                                        class="white-space-nowrap px-3 m-1 cursor-pointer my-1" severity="info"></Tag>
+                                    </div>
+                                    <div v-else>
+                                      {{ getItemValueByName(getFieldDetailsById(id).name) ? getItemValueByName(getFieldDetailsById(id).name) : '' }}
+                                    </div>
                                   </div>
                                 </div>
                               </div>
                             </div>
                           </div>
                         </div>
-                      </div>
-                    </div>
-                  </Panel>
+                    </Panel>
+                  </div>
                 </div>
               </div>
             </div>
