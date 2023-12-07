b4410980aba57f102a70b59caa6efbe22bde99b3
alexela8882 <alexela8882@gmail.com>
2023-12-07 10:20:29 +0800

    bug fix in details page

--- src/views/pages/modules/pages/detail.vue
+++ src/views/pages/modules/pages/detail.vue
@@ -1,32 +1,32 @@
 <script setup>
 // imports
 import { defineAsyncComponent } from 'vue'
+// components
+import ShowDetail from '../../../../components/modules/Page/ShowDetail.vue'
 
 // refs
 // components
 // const ShowDetail = defineAsyncComponent(() =>
 //   import('../../../../components/modules/Page/ShowDetail.vue')
 // )
-const ShowDetail = defineAsyncComponent({
-  loader: () => import('@/components/modules/Page/ShowDetail.vue'),
-  // A component to use while the async component is loading
-  // loadingComponent: LoadingComponent,
-  // Delay before showing the loading component. Default: 200ms.
-  delay: 200,
+// const ShowDetail = defineAsyncComponent({
+//   loader: () => import('@/components/modules/Page/ShowDetail.vue'),
+//   // A component to use while the async component is loading
+//   // loadingComponent: LoadingComponent,
+//   // Delay before showing the loading component. Default: 200ms.
+//   delay: 200,
 
-  // A component to use if the load fails
-  // errorComponent: ErrorComponent,
-  // The error component will be displayed if a timeout is
-  // provided and exceeded. Default: Infinity.
-  timeout: 3000
-})
+//   // A component to use if the load fails
+//   // errorComponent: ErrorComponent,
+//   // The error component will be displayed if a timeout is
+//   // provided and exceeded. Default: Infinity.
+//   timeout: 3000
+// })
 </script>
 
 <template>
   <div>
-    <Suspense>
-      <ShowDetail />
-    </Suspense>
+    <ShowDetail />
   </div>
 </template>
 
