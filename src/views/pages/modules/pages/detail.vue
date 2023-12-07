ef44cb17128d864d4448d0c8dc022ca377b85eeb
alexela8882 <alexela8882@gmail.com>
2023-12-07 09:55:01 +0800

    bug fix in showdetail component

--- src/views/pages/modules/pages/detail.vue
+++ src/views/pages/modules/pages/detail.vue
@@ -4,9 +4,22 @@ import { defineAsyncComponent } from 'vue'
 
 // refs
 // components
-const ShowDetail = defineAsyncComponent(() =>
-  import('../../../../components/modules/Page/ShowDetail.vue')
-)
+// const ShowDetail = defineAsyncComponent(() =>
+//   import('../../../../components/modules/Page/ShowDetail.vue')
+// )
+const ShowDetail = defineAsyncComponent({
+  loader: () => import('../../../../components/modules/Page/ShowDetail.vue'),
+  // A component to use while the async component is loading
+  // loadingComponent: LoadingComponent,
+  // Delay before showing the loading component. Default: 200ms.
+  delay: 200,
+
+  // A component to use if the load fails
+  // errorComponent: ErrorComponent,
+  // The error component will be displayed if a timeout is
+  // provided and exceeded. Default: Infinity.
+  timeout: 3000
+})
 </script>
 
 <template>
