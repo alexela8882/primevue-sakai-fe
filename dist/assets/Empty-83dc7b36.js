44ae9decd978f325f242fb6fa5a9f2770f33cffa
alexela8882 <alexela8882@gmail.com>
2023-12-19 10:18:13 +0800

    production build

---
+++ dist/assets/Empty-83dc7b36.js
@@ -0,0 +1,1 @@
+import{D as e,o as s,j as o,e as t}from"./index-31ef47e2.js";const c={},a={className:"card"},n=t("h5",null,"Empty Page",-1),r=t("p",null,"Use this page to start from scratch and place your custom content.",-1),_=[n,r];function d(l,i){return s(),o("div",a,_)}const p=e(c,[["render",d]]);export{p as default};
