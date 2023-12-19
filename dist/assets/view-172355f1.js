1f29fc0cb0c8948a575284fa27acc7ceca18eaad
alexela8882 <alexela8882@gmail.com>
2023-12-19 10:31:13 +0800

    production build

--- dist/assets/view-172355f1.js
+++
@@ -1,1 +0,0 @@
-import{o,d as t,w as a,S as s,f as _,i as n,C as r,_ as c}from"./index-f9d124e0.js";const l={__name:"view",setup(u){const e=r(()=>c(()=>import("./Show-ded647d1.js"),["assets/Show-ded647d1.js","assets/index-f9d124e0.js","assets/index-75db8d00.css","assets/DataTableLoader-e27bb8ec.js"]));return(i,p)=>(o(),t(s,null,{default:a(()=>[_(n(e))]),_:1}))}};export{l as default};
---
+++ dist/assets/view-f6b229b2.js
@@ -0,0 +1,1 @@
+import{D as e,o as c,j as o}from"./index-9fb1c987.js";const r={};function t(n,s){return c(),o("div")}const _=e(r,[["render",t]]);export{_ as default};
