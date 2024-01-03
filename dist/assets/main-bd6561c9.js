81195c0905c87bed8faa061d7c1a0b9a9fad210d
alexela8882 <alexela8882@gmail.com>
2024-01-03 10:55:02 +0800

    production build

---
+++ dist/assets/main-bd6561c9.js
@@ -0,0 +1,1 @@
+import{D as o,c as r,o as c,d as n}from"./index-c3bf9430.js";const t={};function s(_,a){const e=r("router-view");return c(),n(e)}const i=o(t,[["render",s]]);export{i as default};
--- dist/assets/view-824f77eb.js
+++
@@ -1,1 +0,0 @@
-import{o,d as t,w as a,S as s,f as _,i as n,C as r,_ as c}from"./index-ba7b65a1.js";const l={__name:"view",setup(u){const e=r(()=>c(()=>import("./Show-56dc22be.js"),["assets/Show-56dc22be.js","assets/index-ba7b65a1.js","assets/index-75db8d00.css","assets/DataTableLoader-b43e253e.js"]));return(i,p)=>(o(),t(s,null,{default:a(()=>[_(n(e))]),_:1}))}};export{l as default};
