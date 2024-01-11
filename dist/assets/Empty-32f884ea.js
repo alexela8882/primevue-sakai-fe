9f24655e18b71fdb8d4dbaedbbcbaf750e0605b9
Alexander Flores <alexela8882@gmail.com>
2024-01-11 12:06:17 +0800

    production build

--- dist/assets/Empty-32f884ea.js
+++
@@ -1,1 +0,0 @@
-import{D as e,o as s,j as o,e as t}from"./index-52a3749d.js";const c={},a={className:"card"},n=t("h5",null,"Empty Page",-1),r=t("p",null,"Use this page to start from scratch and place your custom content.",-1),_=[n,r];function d(l,i){return s(),o("div",a,_)}const p=e(c,[["render",d]]);export{p as default};
--- dist/assets/view-29b1394b.js
+++
@@ -1,1 +0,0 @@
-import{o,d as t,w as a,S as s,f as _,i as n,C as r,_ as c}from"./index-52a3749d.js";const l={__name:"view",setup(u){const e=r(()=>c(()=>import("./Show-301925e5.js"),["assets/Show-301925e5.js","assets/index-52a3749d.js","assets/index-86c8a9da.css","assets/DataTableLoader-b5ff0c44.js"]));return(i,p)=>(o(),t(s,null,{default:a(()=>[_(n(e))]),_:1}))}};export{l as default};
