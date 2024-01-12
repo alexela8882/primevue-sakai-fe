20f84f4c5dee6681e187ae68c8d7a98196569192
Alexander Flores <alexela8882@gmail.com>
2024-01-12 12:35:28 +0800

    production build

---
+++ dist/assets/view-21d5d186.js
@@ -0,0 +1,1 @@
+import{o,d as t,w as a,S as s,f as _,i as n,C as r,_ as c}from"./index-e7be2f3c.js";const l={__name:"view",setup(u){const e=r(()=>c(()=>import("./Show-f2f72a3c.js"),["assets/Show-f2f72a3c.js","assets/index-e7be2f3c.js","assets/index-86c8a9da.css","assets/DataTableLoader-cf584b0e.js"]));return(i,p)=>(o(),t(s,null,{default:a(()=>[_(n(e))]),_:1}))}};export{l as default};
---
+++ dist/assets/view-6e949696.js
@@ -0,0 +1,1 @@
+import{D as e,o as c,j as o}from"./index-e7be2f3c.js";const r={};function t(n,s){return c(),o("div")}const _=e(r,[["render",t]]);export{_ as default};
