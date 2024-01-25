cb2243ea3e7ada44eeb4b4578f808743acb0babc
Alexander Flores <alexela8882@gmail.com>
2024-01-25 11:42:25 +0800

    production build

--- dist/assets/view-495c9aea.js
+++
@@ -1,1 +0,0 @@
-import{o,d as t,w as a,S as s,f as _,i as n,C as r,_ as c}from"./index-7dc67f08.js";const l={__name:"view",setup(u){const e=r(()=>c(()=>import("./Show-ad1dbb22.js"),["assets/Show-ad1dbb22.js","assets/index-7dc67f08.js","assets/index-d1288d3d.css","assets/DataTableLoader-2dbaa591.js"]));return(i,p)=>(o(),t(s,null,{default:a(()=>[_(n(e))]),_:1}))}};export{l as default};
