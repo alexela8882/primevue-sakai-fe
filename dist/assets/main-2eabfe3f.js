b177d68ea7639a15e1f70b6e803c6ae8ece45b1a
Alexander Flores <alexela8882@gmail.com>
2024-01-26 10:52:51 +0800

    production build

---
+++ dist/assets/main-2eabfe3f.js
@@ -0,0 +1,1 @@
+import{J as t,c as r,o as n,d as s,i as a}from"./index-954446e7.js";const m={__name:"main",setup(c){const e=t();return(u,_)=>{const o=r("router-view");return n(),s(o,{key:a(e).fullPath})}}};export{m as default};
