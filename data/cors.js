57a31e326ee0139f471f1455334ad855c961bfc0
Alexander Flores <alexela8882@gmail.com>
2024-01-26 10:01:29 +0800

    cors middleware updated to work with json-server

--- data/cors.js
+++ data/cors.js
@@ -1,4 +1,9 @@
-// cors.js
+const jsonServer = require('json-server')
+    
+// Import the library:
 const cors = require('cors')
 
-module.exports = cors()
+const server = jsonServer.create()       
+        
+// Then use it before your routes are set up:
+server.use(cors())
