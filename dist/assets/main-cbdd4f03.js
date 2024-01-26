9e6d68dc230dda931b27c2c62adc69fa88118695
Alexander Flores <alexela8882@gmail.com>
2024-01-26 10:33:50 +0800

    production build

--- dist/assets/main-cbdd4f03.js
+++
@@ -1,1 +0,0 @@
-import{J as t,c as r,o as n,d as s,i as a}from"./index-37e7027c.js";const m={__name:"main",setup(c){const e=t();return(u,_)=>{const o=r("router-view");return n(),s(o,{key:a(e).fullPath})}}};export{m as default};
---
+++ dist/assets/view-ec4735a9.js
@@ -0,0 +1,1 @@
+import{o,d as t,w as a,S as s,f as _,i as n,C as r,_ as c}from"./index-26e6ef51.js";const l={__name:"view",setup(u){const e=r(()=>c(()=>import("./Show-ba755280.js"),["assets/Show-ba755280.js","assets/index-26e6ef51.js","assets/index-d1288d3d.css","assets/DataTableLoader-4c07dab4.js"]));return(i,p)=>(o(),t(s,null,{default:a(()=>[_(n(e))]),_:1}))}};export{l as default};
