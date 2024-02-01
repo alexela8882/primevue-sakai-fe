6a09f8bc963fd377a090f302800128e4ace128a8
Alexander Flores <alexela8882@gmail.com>
2024-02-01 13:02:01 +0800

    revert changes

--- src/stores/base.js
+++ src/stores/base.js
@@ -4,7 +4,7 @@ import { ref, computed } from 'vue'
 
 export const useBaseStore = defineStore('baseStore', () => {
   // states
-  const jsonDbUrl = ref('https://api.npoint.io/8c1df7607a7eabdf5a13')
+  const jsonDbUrl = ref('http://localhost:3000')
   const backendUrl = ref('http://localhost:80')
   const configBar = ref(false)
   const routerLoading = ref(false)
