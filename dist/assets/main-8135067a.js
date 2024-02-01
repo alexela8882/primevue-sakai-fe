a4ac5bd195d411ac336c4b4908ae47ae0aa430db
Alexander Flores <alexela8882@gmail.com>
2024-02-01 13:03:35 +0800

    production build

--- dist/assets/main-8135067a.js
+++
@@ -1,1 +0,0 @@
-import{D as o,c as r,o as c,d as n}from"./index-767066e8.js";const t={};function s(_,a){const e=r("router-view");return c(),n(e)}const i=o(t,[["render",s]]);export{i as default};
--- dist/assets/view-c6a42ea3.js
+++
@@ -1,1 +0,0 @@
-import{D as e,o as c,j as o}from"./index-767066e8.js";const r={};function t(n,s){return c(),o("div")}const _=e(r,[["render",t]]);export{_ as default};
