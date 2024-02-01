520fc50702939546223f9704ac539e36a4f1c64d
Alexander Flores <alexela8882@gmail.com>
2024-02-01 11:58:24 +0800

    production build

--- dist/assets/view-20c9abad.js
+++
@@ -1,1 +0,0 @@
-import{o,d as t,w as a,S as s,f as _,i as n,C as r,_ as c}from"./index-a038a467.js";const l={__name:"view",setup(u){const e=r(()=>c(()=>import("./Show-660bf355.js"),["assets/Show-660bf355.js","assets/index-a038a467.js","assets/index-d1288d3d.css","assets/DataTableLoader-5e63c07f.js"]));return(i,p)=>(o(),t(s,null,{default:a(()=>[_(n(e))]),_:1}))}};export{l as default};
