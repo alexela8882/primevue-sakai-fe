1b7392d3facd9e3cd2701ba2ded1185992b45bd0
s_layson <sharmaine.layson@escolifesciences.com>
2024-02-12 15:52:14 +0800

    Update

--- src/components/modules/Form/Form.vue
+++ src/components/modules/Form/Form.vue
@@ -1,16 +1,22 @@
 <script setup>
-    import { ref  } from 'vue'
+    import { ref, onMounted   } from 'vue'
     import { useRouter } from 'vue-router'
     import { storeToRefs } from 'pinia'
     import { useToast } from "primevue/usetoast"
     import axios from 'axios'
-    import { fill } from 'lodash'
+    import { fill,cloneDeep } from 'lodash'
 
     import { useModuleStore } from '../../../stores/modules'
 
+    const props = defineProps({
+        config: Object
+    })
+
     const tempFields = ref(fill(Array(10),1))
+    const formLoading = ref(true)
     
     onMounted(async () => {
+        console.log(cloneDeep(props.config.module))
     })
 </script>
 <template>
