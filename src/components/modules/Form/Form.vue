97dcfb446db09ef53a09ba21daa61c81323bba0a
s_layson <sharmaine.layson@escolifesciences.com>
2024-02-12 15:52:14 +0800

    Update

--- src/components/modules/Form/Form.vue
+++ src/components/modules/Form/Form.vue
@@ -1,79 +1,23 @@
 <script setup>
-    import { ref, onMounted, defineAsyncComponent,provide } from 'vue'
+    import { ref, onMounted   } from 'vue'
     import { useRouter } from 'vue-router'
     import { storeToRefs } from 'pinia'
     import { useToast } from "primevue/usetoast"
     import axios from 'axios'
-    import { fill,cloneDeep,filter, forEach,map,value,chain, includes,endsWith, reduce,get } from 'lodash'
+    import { fill,cloneDeep } from 'lodash'
 
     import { useModuleStore } from '../../../stores/modules'
-    import { useFormDataStore } from '../../../stores/forms'
-    
-
-    const Field = defineAsyncComponent(() => import('@/components/modules/Form/Field.vue'))
-    const FormPanel = defineAsyncComponent(() => import('@/components/modules/Form/FormPanel.vue'))
 
     const props = defineProps({
         config: Object
     })
-    const formDataStore = useFormDataStore()
-    const { fetchPicklist } = formDataStore
+
     const tempFields = ref(fill(Array(10),1))
     const formLoading = ref(true)
-    const formData = ref({
-        'fields':[],
-        'panels':[],
-        'values':{
-            'main':{},
-            'mutable':[]
-        },
-        'errors':{
-            'main':{},
-            'mutable':[]
-        }
-    })
-
-    const hiddenPanels = ref([])
     
     onMounted(async () => {
-        formData.value.fields = props.config.module.fields
-        formData.value.panels = props.config.module.panels
-        formData.value.values.main = reduce(formData.value.fields, function(res,val,i){
-            res[val.name] = (val.field_type.name=='number' || val.field_type.name=='percentage' || val.field_type.name=='currency') ? 0 : ((val.field_type.name=='boolean') ? true : ((get(val.rules,'ms_dropdown',false) ? [] : null) ))
-            return res
-        },{})
-        await fetchPicklist()
-        initialize();
+        console.log(cloneDeep(props.config.module))
     })
-
-    function initialize(){
-        let quickAddFields = chain(formData.value.fields).filter({'quick':true}).map('_id').value()
-
-        //get quick add panels
-        forEach(formData.value.panels, function(panel, panelI){
-            formData.value.panels[panelI]['quick'] = false
-            forEach(panel.sections, function(section, sectionI){
-                formData.value.panels[panelI]['sections'][sectionI]['quick'] = false
-                let allF = section.field_ids[0]
-                if(section.field_ids.length>1){
-                    allF.concat(section.field_ids[1])
-                }
-                let tmpFields = filter(allF, function(f){
-                    if(includes(quickAddFields, f)){
-                        return true
-                    }
-                })
-                if(tmpFields.length > 0){
-                    formData.value.panels[panelI]['quick'] = true
-                    formData.value.panels[panelI]['sections'][sectionI]['quick'] = true
-                }
-            })
-        })
-
-        formLoading.value = false
-    }
-
-    provide('form', formData)
 </script>
 <template>
     <Suspense v-if="!formLoading">
