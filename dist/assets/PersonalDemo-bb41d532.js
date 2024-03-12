158c957b7ad4751597197d593a60000c1ba79974
Alexander Flores <alexela8882@gmail.com>
2024-03-12 15:34:16 +0800

    production build

--- dist/assets/PersonalDemo-bb41d532.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as s,j as o,e}from"./index-4d8eaf5a.js";const n={},c={class:"flex align-items-center py-5 px-3"},a=e("i",{class:"pi pi-fw pi-user mr-2 text-2xl"},null,-1),r=e("p",{class:"m-0 text-lg"},"Personal Component Content via Child Route",-1),_=[a,r];function i(l,p){return s(),o("div",c,_)}const m=t(n,[["render",i]]);export{m as default};
