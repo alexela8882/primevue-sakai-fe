00ae10b5b8fe9d19a36e207ed63dc708970fa219
Alexander Flores <alexela8882@gmail.com>
2024-03-21 17:32:46 +0800

    production build

---
+++ dist/assets/ConfirmationDemo-5427d5f3.js
@@ -0,0 +1,1 @@
+import{_ as t,o,j as s,e}from"./index-f956d2f5.js";const n={},c={class:"flex align-items-center py-5 px-3"},i=e("i",{class:"pi pi-fw pi-check mr-2 text-2xl"},null,-1),a=e("p",{class:"m-0 text-lg"},"Confirmation Component Content via Child Route",-1),_=[i,a];function r(l,m){return o(),s("div",c,_)}const d=t(n,[["render",r]]);export{d as default};
--- dist/assets/main-38b098e7.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as r,o as c,d as n}from"./index-dd487e2b.js";const t={};function s(_,a){const e=r("router-view");return c(),n(e)}const i=o(t,[["render",s]]);export{i as default};
